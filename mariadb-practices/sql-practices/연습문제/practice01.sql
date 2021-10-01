00655401cb8a75c50f8124513be8fd96fb074723
kjw222 <whffu78@naver.com>
2021-10-01 20:15:00 +0900

    "20211001 연습문제 1 완료, 2 푸는 중"

--- mariadb-practices/sql-practices/연습문제/practice01.sql
+++ mariadb-practices/sql-practices/연습문제/practice01.sql
@@ -10,32 +10,63 @@ where emp_no = 10944;
 #전체직원의 다음 정보를 조회하세요. 가장 선임부터 출력이 되도록 하세요. 출력은 이름, 성별,  입사일 순서이고 “이름”, “성별”, “입사일로 컬럼 이름을 대체해 보세요.
 select concat(first_name, ' ',last_name)as 이름, gender as 성별 , hire_date as 입사일
 from employees
-where
+order by hire_date asc;
 
 
 #문제3.
 #여직원과 남직원은 각 각 몇 명이나 있나요?
+select count( case when gender = 'f' then 1 END) as '여직원수' ,
+		count( case when gender = 'm' then 1 END) as '남직원수'
+from employees;
+
 
 #문제4.
 #현재 근무하고 있는 직원 수는 몇 명입니까? (salaries 테이블을 사용합니다.) 
+select count( case when to_date = '9999-01-01' then 1 END) as '현재 근무하고 있는 직원의 수'
+from salaries;
+
 
 #문제5.
 #부서는 총 몇 개가 있나요?
+select count(dept_no) as '부서 수'
+from departments;
 
 #문제6.
 #현재 부서 매니저는 몇 명이나 있나요?(역임 매너저는 제외)
+select count(distinct emp_no)
+from dept_manager;
 
 #문제7.
 #전체 부서를 출력하려고 합니다. 순서는 부서이름이 긴 순서대로 출력해 보세요.
+select dept_name
+from departments
+order by char_length(dept_name) desc;
+
 
 #문제8.
 #현재 급여가 120,000이상 받는 사원은 몇 명이나 있습니까?
+select count(distinct emp_no) as '급여가 120,000 이상인 사원 수'
+from salaries
+where salary >= 120000
+and to_date = '9999-01-01';
 
 #문제9.
 #어떤 직책들이 있나요? 중복 없이 이름이 긴 순서대로 출력해 보세요.
+select distinct title as '직책'
+from titles
+order by char_length(title) desc;
 
 #문제10
-#현재 Enginner 직책의 사원은 총 몇 명입니까?
+#현재 Engineer 직책의 사원은 총 몇 명입니까?
+select count(distinct emp_no) as 'Engineer 직책의 사원 수'
+from titles
+where title = 'Engineer'
+and to_date = '9999-01-01';
+
 
 #문제11
 #사번이 13250(Zeydy)인 지원이 직책 변경 상황을 시간순으로 출력해보세요.
+select emp_no, title, from_date, to_date
+from titles
+where emp_no = 13250
+order by from_date asc;
